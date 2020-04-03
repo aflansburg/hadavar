7545b9aea5dd7c27ce22370e6cf07f08f95ce666
Abram Flansburg <abeflansburg@gmail.com>
2020-04-02 22:39:19 -0500

    fix issues

--- pages/_document.js
+++ pages/_document.js
@@ -21,6 +21,13 @@ export default class MyDocument extends Document {
           />
         </Head>
         <body>
+          <div id="fb-root"></div>
+          <script
+            async
+            defer
+            crossOrigin="anonymous"
+            src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v6.0"
+          ></script>
           <Main />
           <NextScript />
         </body>
--- src/Reader/PassageReader.js
+++ src/Reader/PassageReader.js
@@ -16,6 +16,7 @@ import {
   BottomNavigation,
   BottomNavigationAction
 } from "@material-ui/core";
+import FacebookIcon from "@material-ui/icons/Facebook";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 import makeStyles from "@material-ui/styles/makeStyles";
 import SvgIcon from "@material-ui/core/SvgIcon";
@@ -92,8 +93,7 @@ function PassageReader({ title, passage, navData }) {
   };
 
   useEffect(() => {
-    const verseNumber =
-      navData.currVerseIndex !== null ? navData.currVerseIndex + 1 : false;
+    const verseNumber = selectedVerse;
     const verseLength = passage.verses.length;
     if (verseNumber && verseNumber <= verseLength) {
       const selVerseElem = document.getElementById(`verse-${verseNumber}`);
@@ -108,7 +108,7 @@ function PassageReader({ title, passage, navData }) {
         setVerseHighlighted(verseNumber);
       }, 250);
     }
-  }, []);
+  }, [selectedVerse, verseHighlighted]);
 
   const handleVerseChange = e => {
     e.preventDefault();
@@ -314,9 +314,33 @@ function PassageReader({ title, passage, navData }) {
         >
           {/* if current chapter index plus 1 less than book length (num of
           chapters) then current book else go to next book via currBookIndex++ */}
-          <Grid container justify="space-between">
+          <Grid container justify="space-between" alignItems="center">
             <Grid item>
               <NavLink op="prev" />
+            </Grid>
+            <Grid
+              item
+              container
+              xs={8}
+              sm={3}
+              justify="center"
+              data-href={window.location.href}
+              data-layout="button"
+              data-size="large"
+            >
+              <IconButton
+                target="_blank"
+                aria-label="share verse to facebook"
+                href={`https://www.facebook.com/sharer/sharer.php?u=${window.location.href}&src=sdkpreparse`}
+                className="fb-xfbml-parse-ignore"
+                style={{ fontSize: 16, color: "#fff" }}
+              >
+                <FacebookIcon
+                  style={{ color: "#fff", marginRight: 4 }}
+                  fontSize="small"
+                />
+                Share Verse To Facebook
+              </IconButton>
             </Grid>
             <Grid item>
               <NavLink op="next" />
